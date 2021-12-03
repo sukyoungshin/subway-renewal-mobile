e86aa62c6e7d92a88ff3ab582ce246f46d620ec4
SuKyoung Shin <sukyoung.dev@gmail.com>
2021-12-04 02:49:03 +0900

    fix : 구글 로그아웃 버튼 클릭 시, 화면에 로그인버튼이 나타나지 않는 현상 (테스트중)
    - 로그아웃 버튼 클릭 시, 현재페이지 리다이렉트 되도록 코드 추가 (local에선 확인되지 않으므로 deploy필요)

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -1,12 +1,15 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { LoginButton, LogoutButton } from '../common/Styled';
 import { RiGoogleLine } from "react-icons/ri";
+import { useNavigate } from 'react-router';
 
 const GoogleLogin = () => {
+  
   // 구글 OAuth 로그인
   const [ isLoggedIn, setIsLoggedIn ] = useState(false); // 로그인여부
   // eslint-disable-next-line
   const [ userInfo, setUserInfo ] = useState(null); // ✅ 로그인 된 유저의 정보저장, 리덕스로 전역관리 해줘야 할 듯
+  const navigate = useNavigate();
 
   const onSignIn = useCallback((googleUser) => {
     const profile = googleUser.getBasicProfile();
@@ -26,6 +29,7 @@ const GoogleLogin = () => {
     document.head.append(script);
     // window 전역객체 사용하여 로그인함수 실행
     window.onSignIn = onSignIn; 
+  // eslint-disable-next-line
   }, [onSignIn]);
 
   const signOut = () => {
@@ -36,6 +40,9 @@ const GoogleLogin = () => {
     });
     setIsLoggedIn(false);
     setUserInfo(null);
+
+    // 로그아웃버튼이 눌리면 페이지 리디렉션
+    navigate('/login');
   };
 
   return (
