41bfe7c8c12f705a7f2e9939382a32b49fae4a94
sukyoung shin <sukyoung.dev@gmail.com>
2021-12-09 20:46:01 +0900

    refactor : 로직수정

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -12,19 +12,22 @@ const Menu = () => {
       window.alert('주문하실 매장을 선택하세요');
       return navigate(-1);
     } // 이전페이지에서 써브웨이 매장 정보가 넘어오지 않았으면 이전 페이지로 강제이동
-
+    
   }, []);
-
-  const { name, phone, url, address, distance } = location.state; // 받아온 정보 destructuring
 
   return (
     <MainWrapper>
-      {/* 임시 출력 -> modify later */}
-      매장 : { name } <br/>
-      phon# : { phone } <br/>
-      addr : { address } <br/>
-      url : { url } <br/>
-      집까지 거리 : { distance } <br/>
+      {
+        location.state && (
+          <>
+          매장 : { location.state.name } <br/>
+          phon# : { location.state.phone } <br/>
+          addr : { location.state.address } <br/>
+          url : { location.state.url } <br/>
+          집까지 거리 : { location.state.distance } <br/>
+          </>
+        )
+      }
     </MainWrapper>
   );
 };
