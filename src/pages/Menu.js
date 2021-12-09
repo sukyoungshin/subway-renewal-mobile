54d8457abcb7626ca97a4981d2ce28128ffa71c7
sukyoung shin <sukyoung.dev@gmail.com>
2021-12-09 20:43:10 +0900

    feat : 선택한 써브웨이 매장정보를 다음페이지로 전달 로직구현

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -12,12 +12,19 @@ const Menu = () => {
       window.alert('주문하실 매장을 선택하세요');
       return navigate(-1);
     } // 이전페이지에서 써브웨이 매장 정보가 넘어오지 않았으면 이전 페이지로 강제이동
-    console.log(location.state); // 정보가 넘어왔으면 콘솔에 출력
-  }, [])
+
+  }, []);
+
+  const { name, phone, url, address, distance } = location.state; // 받아온 정보 destructuring
 
   return (
     <MainWrapper>
-      menu
+      {/* 임시 출력 -> modify later */}
+      매장 : { name } <br/>
+      phon# : { phone } <br/>
+      addr : { address } <br/>
+      url : { url } <br/>
+      집까지 거리 : { distance } <br/>
     </MainWrapper>
   );
 };
