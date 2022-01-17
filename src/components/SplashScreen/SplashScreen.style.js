6cbd446ab546178af2b5ae3f0e4e0d5bac182573
sukyoung shin <sukyoung.dev@gmail.com>
2022-01-18 04:07:10 +0900

    refactor : 데스크탑 고려 style 단위 변경 (vw -> %)

--- src/components/SplashScreen/SplashScreen.style.js
+++ src/components/SplashScreen/SplashScreen.style.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const WrapperStyled = styled.main`
   padding: 8px 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: var(--color-green-dark);
 
   display: inline-flex;
